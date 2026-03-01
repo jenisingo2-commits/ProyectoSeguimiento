e7567a2b315d6f5a42f936113d2cced5f55dddbb
jenisingo2-commits <jenisingo2@gmail.com>
2026-03-01 15:44:34 -0500

    empleados

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('registro/', views.registro, name='registro'),
     path('logout/', views.logout_view, name='logout'),
+     path('dashboard/', views.dashboard, name='dashboard'),
 ]
