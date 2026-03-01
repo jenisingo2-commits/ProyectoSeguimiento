3d2e9a980ccecdd96938d434117de14c1381b0e9
jenisingo2-commits <jenisingo2@gmail.com>
2026-03-01 15:55:04 -0500

    .

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=100)
+    # otros campos...
